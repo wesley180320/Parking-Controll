da94d6c869bd8488ca188cea3ab01d92efacc3e4
Wesley Estevão da Slva de Souza <wesley.estevao@maiscode.com.br>
2022-10-05 16:53:40 -0400

    Ajustes - mobile

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -6,7 +6,6 @@ const Navbar = () => {
   return (
     
     <nav className="navbar navbar-expand-lg bg-dark sticky-top">
-
       <div className="container-fluid">
         <div>
           <a href="/"><p> Parking <span>Controll</span></p> <br /></a>
@@ -14,8 +13,7 @@ const Navbar = () => {
         </div>
         <nav>
           <Link className="link btn btn-outline-danger" to={"/"}> Home </Link>
-          <Link className="link btn btn-outline-danger " to={"/Cadastrar"}>Cadastrar</Link>
-
+          <Link className="link btn btn-outline-danger " to={"/Cadastrar"}>Acessar Sistema</Link>
         </nav>
       </div>
     </nav>
